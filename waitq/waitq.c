9f98b9576b20dcb24d8bfd133fd54b61e94bf560
Niko <niko.nastonen@gmail.com>
2024-06-17 08:40:07 +0000

    added driver context

--- waitq/waitq.c
+++ waitq/waitq.c
@@ -12,15 +12,16 @@
 
 #define NAME_LEN 20
 
-static struct device *dev;
-static struct kmem_cache *g_mem_cache;
-static atomic_t cnt = ATOMIC_INIT(1);
-static struct task_struct *waitq_task;
-static atomic_t data_ready = ATOMIC_INIT(0);
-
-static DECLARE_WAIT_QUEUE_HEAD(wee_wait);
-static LIST_HEAD(head_node);
-static DEFINE_MUTEX(list_mtx);
+static struct drv_ctx {
+	struct device *dev;
+	struct kmem_cache *mem_cache;
+	struct task_struct *waitq_task;
+	struct list_head head_node;
+	struct mutex list_mtx;
+	wait_queue_head_t wee_wait;
+	atomic_t cnt;
+	atomic_t data_ready;
+} *ctx;
 
 struct identity {
 	struct list_head list;
@@ -33,7 +34,7 @@ static int identity_create(const char *name, int id)
 {
 	struct identity *tmp = NULL;
 
-	tmp = kmem_cache_alloc(g_mem_cache, GFP_KERNEL);
+	tmp = kmem_cache_alloc(ctx->mem_cache, GFP_KERNEL);
 	if (unlikely(!tmp))
 		return -ENOMEM;
 
@@ -41,11 +42,12 @@ static int identity_create(const char *name, int id)
 	tmp->id = id;
 	tmp->busy = false;
 
-	mutex_lock(&list_mtx);
-	list_add_tail(&tmp->list, &head_node);
-	mutex_unlock(&list_mtx);
+	mutex_lock(&ctx->list_mtx);
+	INIT_LIST_HEAD(&tmp->list);
+	list_add_tail(&tmp->list, &ctx->head_node);
+	mutex_unlock(&ctx->list_mtx);
 
-	dev_info(dev, "Added node %s to the list\n", tmp->name);
+	dev_info(ctx->dev, "Added node %s to the list\n", tmp->name);
 
 	return 0;
 }
@@ -54,14 +56,14 @@ static struct identity *identity_find(int id)
 {
 	struct identity *curr, *found = NULL;
 
-	mutex_lock(&list_mtx);
-	list_for_each_entry(curr, &head_node, list) {
+	mutex_lock(&ctx->list_mtx);
+	list_for_each_entry(curr, &ctx->head_node, list) {
 		if (curr->id == id) {
 			found = curr;
 			break;
 		}
 	}
-	mutex_unlock(&list_mtx);
+	mutex_unlock(&ctx->list_mtx);
 
 	return found;
 }
@@ -70,11 +72,11 @@ static struct identity *identity_get(void)
 {
 	struct identity *ret;
 
-	mutex_lock(&list_mtx);
-	ret = list_first_entry_or_null(&head_node, struct identity, list);
+	mutex_lock(&ctx->list_mtx);
+	ret = list_first_entry_or_null(&ctx->head_node, struct identity, list);
 	if (ret)
 		list_del(&ret->list);
-	mutex_unlock(&list_mtx);
+	mutex_unlock(&ctx->list_mtx);
 
 	return ret;
 }
@@ -83,21 +85,21 @@ static void identity_destroy(int id)
 {
 	struct identity *curr, *tmp, *found = NULL;
 
-	mutex_lock(&list_mtx);
-	list_for_each_entry_safe(curr, tmp, &head_node, list) {
+	mutex_lock(&ctx->list_mtx);
+	list_for_each_entry_safe(curr, tmp, &ctx->head_node, list) {
 		if (curr->id == id) {
 			found = curr;
 			list_del(&curr->list);
 			break;
 		}
 	}
-	mutex_unlock(&list_mtx);
+	mutex_unlock(&ctx->list_mtx);
 
 	if (found) {
-		dev_dbg(dev, "Destroyed %d\n", found->id);
-		kmem_cache_free(g_mem_cache, found);
+		dev_dbg(ctx->dev, "Destroyed %d\n", found->id);
+		kmem_cache_free(ctx->mem_cache, found);
 	} else {
-		dev_dbg(dev, "Tried to destroy %d but not found\n", id);
+		dev_dbg(ctx->dev, "Tried to destroy %d but not found\n", id);
 	}
 }
 
@@ -105,43 +107,43 @@ static void list_destroy(void)
 {
 	struct identity *curr, *tmp;
 
-	mutex_lock(&list_mtx);
-	list_for_each_entry_safe(curr, tmp, &head_node, list) {
+	mutex_lock(&ctx->list_mtx);
+	list_for_each_entry_safe(curr, tmp, &ctx->head_node, list) {
 		list_del(&curr->list);
-		kmem_cache_free(g_mem_cache, curr);
+		kmem_cache_free(ctx->mem_cache, curr);
 	}
-	mutex_unlock(&list_mtx);
+	mutex_unlock(&ctx->list_mtx);
 }
 
 static int wee_kthread(void *data)
 {
 	while (!kthread_should_stop()) {
-		/* Sleep until data becomes ready and notified. */
-		wait_event_interruptible(wee_wait, kthread_should_stop() || atomic_read(&data_ready) != 0);
+		/* Sleep until data becomes ready or stop thread. */
+		wait_event_interruptible(ctx->wee_wait, kthread_should_stop() || atomic_read(&ctx->data_ready) != 0);
 
 		if (kthread_should_stop()) {
-			dev_dbg(dev, "kthread_should_stop()\n");
+			dev_dbg(ctx->dev, "kthread_should_stop()\n");
 			break;
 		}
 
-		if (likely(atomic_read(&data_ready) != 0)) {
-			atomic_dec(&data_ready);
+		if (likely(atomic_read(&ctx->data_ready) != 0)) {
+			atomic_dec(&ctx->data_ready);
 			
-			dev_dbg(dev, "kthread woke up, data is ready\n");
+			dev_dbg(ctx->dev, "kthread woke up, data is ready\n");
 
 			struct identity *idnt = identity_get();
 			if (unlikely(!idnt)) {
-				dev_dbg(dev, "list was empty, should not happen\n");
+				dev_dbg(ctx->dev, "list was empty, should not happen\n");
 				continue;
 			}
-			dev_dbg(dev, "got identity: %s number %d\n", idnt->name, idnt->id);
+			dev_dbg(ctx->dev, "got identity: %s number %d\n", idnt->name, idnt->id);
 
 			/* sleep */
 			ssleep(2);
 
-			kmem_cache_free(g_mem_cache, idnt);
+			kmem_cache_free(ctx->mem_cache, idnt);
 		} else {
-			dev_dbg(dev, "kthread woke up, DATA NOT READY, INTERRUPT\n");
+			dev_dbg(ctx->dev, "kthread woke up, DATA NOT READY, INTERRUPT\n");
 		}
 	}
 
@@ -155,24 +157,24 @@ static ssize_t write_miscdev(struct file *filp, const char __user *ubuf,
 	void *kbuf; 
 
 	if (count >= NAME_LEN) {
-		dev_warn(dev, "Name exceeds max allowed 19 chars! Shrinking.\n");
+		dev_warn(ctx->dev, "Name exceeds max allowed 19 chars! Shrinking.\n");
 		count = NAME_LEN - 1;
 	}
 
 	kbuf = kvzalloc(count + 1, GFP_KERNEL);
 
 	if (copy_from_user(kbuf, ubuf, count)) {
-		dev_warn(dev, "copy_from_user() failed, returning\n");
+		dev_warn(ctx->dev, "copy_from_user() failed, returning\n");
 		kvfree(kbuf);
 		return -EFAULT;
 	}
 
 	/* Add to tail of the list. */
-	identity_create(kbuf, atomic_fetch_add(1, &cnt));
+	identity_create(kbuf, atomic_fetch_add(1, &ctx->cnt));
 
 	/* Wake up the wait queue. */
-	atomic_inc(&data_ready);
-	wake_up_interruptible(&wee_wait);
+	atomic_inc(&ctx->data_ready);
+	wake_up_interruptible(&ctx->wee_wait);
 
 	return ret;
 }
@@ -210,25 +212,33 @@ static int __init waitq_init(void)
 		return ret;
 	}
 
-	/* Get the device pointer for this device. */
-	dev = llkd_miscdev.this_device;
+	/* Initialize driver context. */
+	ctx = devm_kzalloc(llkd_miscdev.this_device, sizeof(struct drv_ctx), GFP_KERNEL);
+	if (!ctx)
+		return -ENOMEM;
+	ctx->dev = llkd_miscdev.this_device;
+	INIT_LIST_HEAD(&ctx->head_node);
+	init_waitqueue_head(&ctx->wee_wait);
+	mutex_init(&ctx->list_mtx);
+	atomic_set(&ctx->cnt, 1);
+	atomic_set(&ctx->data_ready, 0);
 
-	dev_info(dev, "LLKD misc driver (major #10, minor #%d) registered,"
+	dev_info(ctx->dev, "LLKD misc driver (major #10, minor #%d) registered,"
 		" dev node is /dev/%s\n", llkd_miscdev.minor, llkd_miscdev.name);
 
 	/* Create memory cache. */
-	g_mem_cache = kmem_cache_create("list_cache",		  // name in /proc/slabinfo etc
+	ctx->mem_cache = kmem_cache_create("list_cache",	  // name in /proc/slabinfo etc
 					sizeof(struct identity),  // (min) size of each object
 					sizeof(long),		  // 0 or sizeof(long) to align to size of word
 					SLAB_HWCACHE_ALIGN,	  // good for performance
 					NULL);
-	if (!g_mem_cache)
+	if (!ctx->mem_cache)
 		return -ENOMEM;
 
 	/* Create kernel thread for wait queue. */
-	waitq_task = kthread_run(wee_kthread, NULL, "waitq_task");
+	ctx->waitq_task = kthread_run(wee_kthread, NULL, "waitq_task");
 	if (IS_ERR(wee_kthread)) {
-		dev_warn(dev, "Failed to create kthread\n");
+		dev_warn(ctx->dev, "Failed to create kthread\n");
 		return PTR_ERR(wee_kthread);
 	}
 
@@ -236,10 +246,10 @@ static int __init waitq_init(void)
 	/* Test data */
 	struct identity *temp;
 
-	identity_create("Alice", atomic_fetch_add(1, &cnt));
-	identity_create("Bob", atomic_fetch_add(1, &cnt));
-	identity_create("Dave", atomic_fetch_add(1, &cnt));
-	identity_create("Gena", atomic_fetch_add(1, &cnt));
+	identity_create("Alice", atomic_fetch_add(1, &ctx->cnt));
+	identity_create("Bob", atomic_fetch_add(1, &ctx->cnt));
+	identity_create("Dave", atomic_fetch_add(1, &ctx->cnt));
+	identity_create("Gena", atomic_fetch_add(1, &ctx->cnt));
 
 	temp = identity_find(3);
 	if (unlikely(temp == NULL))
@@ -267,17 +277,17 @@ static void __exit waitq_exit(void)
 {
 	list_destroy();
 
-	if (likely(list_empty(&head_node)))
-		dev_info(dev, "list is empty now\n");
+	if (likely(list_empty(&ctx->head_node)))
+		dev_info(ctx->dev, "list is empty now\n");
 	else
-		dev_info(dev, "list is left NON-empty\n");
+		dev_info(ctx->dev, "list is left NON-empty\n");
 
-	if (waitq_task)
-		kthread_stop(waitq_task);
+	if (ctx->waitq_task)
+		kthread_stop(ctx->waitq_task);
 
 	pr_info("kthread_stop() called\n");
 
-	kmem_cache_destroy(g_mem_cache);
+	kmem_cache_destroy(ctx->mem_cache);
 
 	pr_info("kmem_cache_destroy() called\n");
 
